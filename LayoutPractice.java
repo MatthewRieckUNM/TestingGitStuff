3ba695d7a868b3a9a0c2ad4c6e51e092b603d5d4
MatthewRieckUNM <mrieck2006@unm.edu>
2025-11-03 11:34:20 -0700

    File Submission

---
+++ LayoutPractice.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LayoutPractice extends JFrame {
+    public LayoutPractice(){
+        super("Testing Layout");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        getContentPane().add(leftPanel, BorderLayout.WEST);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY,5,true));
+        Button button1 = new Button("Button1");
+        JPanel blankPanel1 = new JPanel();
+        JPanel blankPanel2 = new JPanel();
+
+        
+
+        leftPanel.add(blankPanel1);
+        leftPanel.add(buttonPanel, BorderLayout.SOUTH);
+        leftPanel.add(blankPanel2);
+        buttonPanel.add(button1, BorderLayout.NORTH);
+        leftPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY,5,true));
+
+        JPanel rightPanel = new JPanel();
+
+        getContentPane().add(rightPanel, BorderLayout.CENTER);
+        rightPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN,10,true));
+
+        setSize(500,500);
+        revalidate();
+        repaint();
+        setVisible(true);
+
+    }
+    public static void main(String[] args){
+        LayoutPractice mine = new LayoutPractice();
+    }
+
+    public void paintComponents(Graphics g){
+        super.paintComponents(g);
+
+        g.setColor(Color.RED);
+        g.drawRect(10,100,30,20);
+    }
+}
+
